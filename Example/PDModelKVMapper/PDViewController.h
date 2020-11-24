544b3044b0ac8716a6ac61194353b015fe666d7c
liang <leiliang4@xdf.cn>
2020-11-24 14:16:16 +0800

    Initial commit

---
+++ Example/PDModelKVMapper/PDViewController.h
@@ -0,0 +1,13 @@
+//
+//  PDViewController.h
+//  PDModelKVMapper
+//
+//  Created by liang on 11/24/2020.
+//  Copyright (c) 2020 liang. All rights reserved.
+//
+
+@import UIKit;
+
+@interface PDViewController : UIViewController
+
+@end
---
+++ Example/PDModelKVMapper/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PDModelKVMapper
+//
+//  Created by liang on 11/24/2020.
+//  Copyright (c) 2020 liang. All rights reserved.
+//
+
+@import UIKit;
+#import "PDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PDAppDelegate class]));
+    }
+}
